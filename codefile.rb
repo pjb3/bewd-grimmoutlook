d9410f35cf6b2fade34d96f0dbeebc599fbcd9ff
GrimmOutlook <grimmoutlook87@gmail.com>
2014-02-25 21:03:54 -0500

    no failures, no errors

--- codefile.rb
+++ codefile.rb
@@ -11,7 +11,7 @@ TAX_RATE = 0.05
 def calculate_total(line_item)
   total_price = 0
     for order in line_item
-      total_price += line_item[:quantity]*line_item[:unit_price]
+      total_price = line_item[:quantity]*line_item[:unit_price]
     end
-  print "$%.2f" % "#{total_price+(total_price*TAX_RATE)}"
+  "$%.2f" % "#{total_price+(total_price*TAX_RATE)}"
 end
--- testfile.rb
+++ testfile.rb
@@ -4,15 +4,15 @@ require './codefile'
 
 class CalculateTotalTest < MiniTest::Unit::TestCase
   def test_total_price_0
-    assert_equal 0, calculate_total(quantity: 0, unit_price: 0)
+    assert_equal "$0.00", calculate_total(quantity: 0, unit_price: 0)
   end
 
   def test_quantity_2
-    assert_equal 4.20, calculate_total(quantity: 2, unit_price: 2)
+    assert_equal "$4.20", calculate_total(quantity: 2, unit_price: 2)
   end
 
   def test_two_orders
-    assert_equal 2.20, calculate_total(quantity: 2, unit_price: 2)
-    assert_equal 7.00, calculate_total(quantity: 3, unit_price: 0.89)
+    assert_equal "$4.20", calculate_total(quantity: 2, unit_price: 2)
+    assert_equal "$2.80", calculate_total(quantity: 3, unit_price: 0.89)
   end
 end
