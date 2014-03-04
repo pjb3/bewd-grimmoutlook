a867281e6d5a8ff65f3240bbba725e79bf59daf2
GrimmOutlook <grimmoutlook87@gmail.com>
2014-03-04 14:48:03 -0500

    Ruby Basics complete - no errors

--- codefile.rb
+++ codefile.rb
@@ -1,24 +1,38 @@
 TAX_RATE = 0.05
 
-#line_item = ([{quantity: 2, unit_price: 2},{quantity: 3, unit_price: 0.89}])
-
-
   def calculate_total(line_item)
+    total_price = 0
     line_item.each do | hsh |
-      hsh.each do | q,up |
-        total_price = hsh[:q]*hsh[:up]
-     end
+        total_price += hsh[:quantity]*hsh[:unit_price]
     end
       "$%.2f" % "#{total_price+(total_price*TAX_RATE)}"
   end
 
+# line_item = [{quantity: 2, unit_price: 2},{quantity: 3, unit_price: 0.89}]
+
+# How do you calculate the total of just one of the hashes in an array
+# of multiple hashes?  Some kind of nested .each iteration I suppose.
+
+# For loop attempt:
+  # def calculate_total(line_item)
+  #   total_price = 0
+  #     for order in line_item
+  #     #  total_price = line_item[:quantity]*line_item[:unit_price]
+  #       total_price = line_item[:quantity, :unit_price]
+  #     end
+  #   "$%.2f" % "#{total_price+(total_price*TAX_RATE)}"
+  # end
 
 
-# def calculate_total(line_item)
-#   total_price = 0
-#     for order in line_item
-#     #  total_price = line_item[:quantity]*line_item[:unit_price]
-#       total_price = line_item[:quantity, :unit_price]
-#     end
-#   "$%.2f" % "#{total_price+(total_price*TAX_RATE)}"
-# end
+# Chris's solution (no need for if statements):
+  # def calculate_total(orders)
+  #   total_price = 0
+  #     if orders.is_a?(hash)
+  #       price +=(orders[:quantity]*orders[:unit_price])
+  #     if orders.is_a?(array)
+  #       orders.each {|item| price += item[:quantity]*item[:unit_price]}
+  #     end
+  #     end
+  #     "$%.2f" % "#{total_price+(total_price*TAX_RATE)}"
+
+  # end
--- testfile.rb
+++ testfile.rb
@@ -4,14 +4,24 @@ require './codefile'
 
 class CalculateTotalTest < MiniTest::Unit::TestCase
   def test_total_price_0
-    assert_equal "$0.00", calculate_total(quantity: 0, unit_price: 0)
+    assert_equal "$0.00", calculate_total([{quantity: 0, unit_price: 0}])
   end
 
   def test_quantity_2
-    assert_equal "$4.20", calculate_total(quantity: 2, unit_price: 2)
+    assert_equal "$4.20", calculate_total([{quantity: 2, unit_price: 2}])
   end
 
   def test_two_orders
-    assert_equal "[$4.20, $2.80]", calculate_total([{quantity: 2, unit_price: 2},{quantity: 3, unit_price: 0.89}])
+    assert_equal "$7.00", calculate_total([{quantity: 2, unit_price: 2},{quantity: 3, unit_price: 0.89}])
   end
+
+# Extra Credit #1
+  def test_empty_array
+    assert_equal "$0.00", calculate_total([])
+  end
+# Extra Credit #2 - Seems the same as test_two_orders
+  def test_mult_orders
+    assert_equal "$157.50", calculate_total([{quantity: 0, unit_price: 0},{quantity: 50, unit_price: 1},{quantity: 40, unit_price: 1},{quantity: 30, unit_price: 1},{quantity: 20, unit_price: 1},{quantity: 10, unit_price: 1}])
+  end
+
 end
